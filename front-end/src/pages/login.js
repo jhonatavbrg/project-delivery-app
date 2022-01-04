4867b341bacb43f61550bc0c03474c1e8ce2d873
Pedrodev1 <pedro.oliveira.dev@outlook.com>
2022-01-04 15:36:11 -0300

    Requisito 2

--- front-end/src/pages/login.js
+++ front-end/src/pages/login.js
@@ -4,7 +4,31 @@ import '../App.css';
 function login() {
   return (
     <div className="App">
-      <span className="lo go">FOI!!!</span>
+      <form>
+        <label htmlFor="input-email">
+          <p>Login</p>
+          <input
+            id="input-email"
+            data-testid="common_login__input-email"
+            type="email"
+          />
+        </label>
+        <label htmlFor="input-password">
+          <p>Senha</p>
+          <input
+            id="input-password"
+            data-testid="common_login__input-password"
+            type="password"
+          />
+        </label>
+        <button data-testid="common_login__button-login" type="button">
+          Login
+        </button>
+        <button data-testid="common_login__button-register" type="button">
+          Ainda não tenho conta
+        </button>
+      </form>
+      <p data-testid="common_login__element-invalid-email">Erro!</p>
     </div>
   );
 }
