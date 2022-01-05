fc25706f1d67fa54530f0a5ff550ba0e6892e831
gabiazevedo <gabiazevedoms@gmail.com>
2022-01-05 17:30:23 -0300

    "fix: Corrige conflitos"

--- back-end/src/api/services/loginServices.js
+++ back-end/src/api/services/loginServices.js
@@ -2,15 +2,6 @@ const md5 = require('md5');
 const { user } = require('../../database/models');
 const { createToken } = require('../middlewares/auth');
 
-<<<<<<< HEAD
-const userLogin = async ({ email, password }) => {
-  const loginUser = await user.findOne({ where: { email, password: md5(password) } });
-  return loginUser;
-};
-
-module.exports = { 
-  userLogin,
-=======
 const login = async ({ email, password }) => {
   const encodedPassword = md5(password);
   const loginUser = await user.findOne({ where: { email, password: encodedPassword } });
@@ -23,5 +14,4 @@ const login = async ({ email, password }) => {
 
 module.exports = { 
   login,
->>>>>>> de03ea968784e493bfe92e3aa689c7e1c6faea3e
 };
