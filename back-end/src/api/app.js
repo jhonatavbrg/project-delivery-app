ee21b9dbcddcdb5e538b17c53d96e21347bc4dfb
Jhonata Braga <jhonatavbrg@icloud.com>
2022-01-06 19:04:38 -0300

    req 17 parcial

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const loginRouter = require('./routes/login');
+const userRouter = require('./routes/user');
 
 const app = express();
 app.use(cors());
@@ -12,6 +13,8 @@ app.use(express.json());
 app.use('/register', registerRouter);
 
 app.use('/login', loginRouter);
+
+app.use('/user', userRouter);
 
 app.get('/coffee', (_req, res) => res.status(418).end());
 
