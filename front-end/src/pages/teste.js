44885152e616701296b9d44af201fcdba1551af7
Jhonata Braga <jhonatavbrg@gmail.com>
2022-01-13 16:08:38 -0300

    base req socket

---
+++ front-end/src/pages/teste.js
@@ -0,0 +1,19 @@
+import React, { useState, useEffect } from 'react';
+import socketClient from 'socket.io-client';
+
+function Teste() {
+  const [string, setString] = useState('');
+
+  useEffect(() => {
+    const socket = socketClient('http://localhost:3001/');
+    socket.on('teste', (value) => setString(value));
+  }, []);
+
+  return (
+    <div>
+      <p>{ string }</p>
+    </div>
+  );
+}
+
+export default Teste;
